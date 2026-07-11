70900e86659c5fdb3228240edccd07babc6ae434
arh812 <arhaan.sultan.200399@standrewsindia.com>
2026-07-11 17:08:41 +0530

    calculator

--- arhaan.py
+++ arhaan.py
@@ -1,12 +1,27 @@
 x=int(input("enter  first number: "))
-y=int(input("enter second number : "))
+y=int(input("enter second number: "))
+op =input("enter operator (+, -, *,/): ")
+if op == "+":
+    result = x + y
+    print(f"Result: {x} + {y} = {result}")
 
-print (x + y)
-print (x - y)
-print (x * y)
-print (x / y)
+elif op == "-":
+    result = x - y
+    print(f"Result: {x} - {y} = {result}")
 
-  
+elif op == "*":
+    result = x * y
+    print(f"Result: {x} * {y} = {result}")
+
+elif op == "/":
+    if y != 0:
+        result = x / y
+        print(f"Result: {x} / {y} = {result}")
+
+
+
+
+
 
 
 
